b53c57dba6173a7e64b0d0d0168049df3a533500
Bayel <bayel.bakytovich@gmail.com>
2020-02-13 15:12:06 +0600

    js static

--- main.js
+++ main.js
@@ -4,4 +4,13 @@ let linksInHeader = document.querySelectorAll("a");
 for (let i = 0; i < linksInHeader.length; i++) {
     const element = linksInHeader[i];
     
+    element.addEventListener("click",function(event){
+        event.preventDefault();
+
+        let ide = element.getAttribute("href");
+       linksInHeader.className = ""
+
+       
+        this.element.className = "active";
+    });
 }
