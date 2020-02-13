5decce6d5a6b43d6be8bddb1a3ef9ce654ea8207
Bayel <bayel.bakytovich@gmail.com>
2020-02-13 14:29:09 +0600

    Add loops

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 let sections = document.querySelectorAll("section");
 let linksInHeader = document.querySelectorAll("a");
+
+for (let i = 0; i < linksInHeader.length; i++) {
+    const element = linksInHeader[i];
+    
+}
