0bdbfb254a0af0f1ad75cfec92ec16a58391fe04
Bayel <bayel.bakytovich@gmail.com>
2020-02-16 17:48:48 +0600

    Deleted function

--- main.js
+++ main.js
@@ -3,17 +3,19 @@ let linksInHeader = document.querySelectorAll("a");
 
 for (let index = 0; index < linksInHeader.length; index++) {
     const element = linksInHeader[index];
-    
-    element.addEventListener("click",function(event){
+
+    element.addEventListener("click", function (event) {
         event.preventDefault();
 
         let ide = element.getAttribute("href");
-       let section = document.querySelector(ide);
+        let section = document.querySelector(ide);
 
-       section.scrollIntoView({
-        behavior:"smooth",
-        block:"start"
-    });
+        section.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
         section.className = "active";
     });
 }
+
+
